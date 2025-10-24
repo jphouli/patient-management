bb16e3c625029d03bdf4545d96d2de77f69e070d
jphouli <jordan_houlihan@yahoo.com>
2025-10-24 14:37:57 -0400

    Fixed simple naming error

--- patient-service/src/main/java/com/pm/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -28,7 +28,7 @@ public class Patient {
     private LocalDate dateOfBirth;
 
     @NotNull
-    private LocalDate registeredBirth;
+    private LocalDate registeredDate;
 
     public UUID getId() {
         return Id;
@@ -70,11 +70,11 @@ public class Patient {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public @NotNull LocalDate getRegisteredBirth() {
-        return registeredBirth;
+    public @NotNull LocalDate getRegisteredDate() {
+        return registeredDate;
     }
 
-    public void setRegisteredBirth(@NotNull LocalDate registeredBirth) {
-        this.registeredBirth = registeredBirth;
+    public void setRegisteredDate(@NotNull LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
     }
 }
