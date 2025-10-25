c5f85b41cb1710167a4094258cb7f33e865b05c5
jphouli <jordan_houlihan@yahoo.com>
2025-10-24 20:50:29 -0400

    Added an auth service module to authenticate users

---
+++ auth-service/src/main/java/com/pm/auth_service/model/User.java
@@ -0,0 +1,53 @@
+package com.pm.auth_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+
+    public @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
