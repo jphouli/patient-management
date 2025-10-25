9a17cfd757fa45d95ca2606337da331258929e5d
jphouli <jordan_houlihan@yahoo.com>
2025-10-24 21:15:04 -0400

    Setting up token validation and setting up the auth service to the api gateway after testing that the functionality works

--- auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
@@ -1,13 +1,17 @@
 package com.pm.auth_service.util;
 
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.Signature;
 import java.util.Base64;
 import java.util.Date;
 
@@ -28,5 +32,18 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (SignatureException exception) {
+            throw new JwtException("Invalid JWT signature");
+        }
+        catch (JwtException exception) {
+            throw new JwtException("Invalid JWT");
+        }
     }
 }
