afe9bd8f4342475e629a160abae4e10a6130432a
jphouli <jordan_houlihan@yahoo.com>
2025-10-24 21:38:58 -0400

    Configured the application.yml file in the api gateway to send api docs for the auth service module and starting an integration test module

--- integration-tests/src/test/java/com/pm/integration_tests/IntegrationTestsApplicationTests.java
+++ integration-tests/src/test/java/com/pm/integration_tests/IntegrationTestsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pm.api_gateway;
+package com.pm.integration_tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class IntegrationTestsApplicationTests {
 
 	@Test
 	void contextLoads() {
